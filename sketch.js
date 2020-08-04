8fd0e7aefa4d63285123123dbf72a7a30199c369
ritwiksaraf28062008 <62437795+ritwiksaraf28062008@users.noreply.github.com>
2020-08-04 18:54:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,11 +9,13 @@ function preload()
 }
 
 function setup() {
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
-  database = firebase.database;
+  canvas = createCanvas(displayWidth - 50,displayHeight - 200);
+  database = firebase.database();
   dog = createSprite(450,250,30,50);
   foodStock = database.ref('Food');
-  foodStock.on("value" , foodStock);
+  foodStock.on("value" , readStock);
+  dog.addImage( "normal" , dogimg);
+  dog.scale = 0.3
        
 }
 
@@ -24,22 +26,38 @@ function setup() {
 
 function draw() { 
   background(46, 139, 87);
-  if(keyWentDown(UP_ARROW)){
+
+  if(keyWentDown(UP_ARROW) && foodS>=0){
     writeStock(foodS);
     dog.addImage(happyDog);
   }
   
+  if(keyWentUp(UP_ARROW) && foodS>=0){
+    dog.addImage(dogimg);
+  }
 
   drawSprites();  
   //add styles here
-  text(foodStock , 250,50)
+  textSize(30);
+  fill("white");
+  text(foodS , 250,50)
 
 }
 
 function writeStock(x){
+  if(x<=0){
+    x = 0;
+  }
+  else{
+    x = x-1
+  }
   database.ref('/').update({
     Food : x
   })
+}
+
+function readStock(data){
+  foodS = data.val();
 }
 
 
